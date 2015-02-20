6d7f3d258c3b13af28d7e5826c5ce0eac3d59712
Iliya Strakovich <iliya@strakovich.com>
2015-02-20 19:02:34 +0200

    get specific salt version from private repo

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
 Role=$1
+Id=$2
+Master=$3
 Folder=$4
+salt_ver=$5
+repo=$6
 
-if [[ "$Role" == "master" ]] ; then
-  Param="-M -N"
-  cd /srv
-elif [[ "$Role" == "minion" ]] ; then
-  Id=$2
-  Master=$3
-  Param="-i ${Id} -A ${Master}"
-else
-  echo "Parameter should be master or minion."
-  exit
-fi
+[[ "$Role" == "master" ]] && Param="-M -N"
+[[ "$Role" == "minion" ]] && Param="-i ${Id} -A ${Master}"
 
-Stat=$(rpm -qa | grep -E -o "salt-master|salt-minion" | awk -F"-" '{print $2}')
-
-if [[ "$Stat" == "master" ]] ; then
-  sudo salt '*' state.highstate
-elif [[ "$Stat" == "minion" ]] ; then
+# check if salt is already installed
+pgrep salt-${Role} &>/dev/null
+if [[ "$?" -eq 0 ]] ; then
   echo "Salt package has been already installed."
+  if [[ "${Role}" == "master" ]] ; then
+    sudo salt '*' state.highstate
+  fi
 else
-  sudo sh ${Folder}/install_salt.sh ${Param} -p vim -p screen -p net-tools -p bash-completion -p vim -p git -p wget
+  sudo sh ${Folder}/install_salt.sh -F ${Param} -g ${repo} -p vim -p screen -p net-tools -p bash-completion -p vim -p git -p wget git v${salt_ver}
 fi
